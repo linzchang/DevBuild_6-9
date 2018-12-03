a63cbc4c45b8bb8c8745bc013544915f62e3255e
Lin-z Chang <lin-zchang@amrock.com>
2018-12-03 16:21:29 -0500

    Repushing views

---
+++ Assessment6/Scripts/Dish.js
@@ -0,0 +1,57 @@
+﻿$(document).ready(function () {
+
+    $("#dish_submit").click(function (event) {
+        var phoneNumber = $("#phone").val();
+        var error_free = true;
+        var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+        var isPhone = re.test(phoneNumber);
+        if (!isPhone) {
+            error_free = false;
+        }
+        if (!error_free) {
+            event.preventDefault();
+            $("#phoneAlert").text('Please enter a valid phone number')
+        }
+    });
+
+    $('#dish').keyup(function () {
+        var dish = $(this).val();
+        //check entry length
+        if (dish.length < 1) {
+            $('#length').removeClass('text-success').addClass('text-danger');
+        } else {
+            $('#length').removeClass('text-danger').addClass('text-success');
+        }
+        //check if entry contains numbers
+        if (dish.match(/^([^0-9]*)$/)) {
+            $('#number').removeClass('text-danger').addClass('text-success');
+        } else {
+            $('#number').removeClass('text-success').addClass('text-danger');
+        }
+    }).focus(function () {
+        $('#dishname').show();
+    }).blur(function () {
+        $('#dishname').hide();
+        });
+
+    $('#dishDescription').keyup(function () {
+        var description = $(this).val();
+        //check entry length
+        if (description.length < 10) {
+            $('#dlength').removeClass('text-success').addClass('text-danger');
+        } else {
+            $('#dlength').removeClass('text-danger').addClass('text-success');
+        }
+        //check if entry contains numbers
+        if (description.match(/^([^0-9]*)$/)) {
+            $('#dnumber').removeClass('text-danger').addClass('text-success');
+        } else {
+            $('#dnumber').removeClass('text-success').addClass('text-danger');
+        }
+    }).focus(function () {
+        $('#description').show();
+    }).blur(function () {
+        $('#description').hide();
+    });
+
+});
---
+++ Assessment6/Scripts/RSVP.js
@@ -0,0 +1,67 @@
+﻿$(document).ready(function () {
+
+    $('#firstName').keyup(function () {
+        var name = $(this).val();
+        //check entry length
+        if (name.length < 1) {
+            $('#length').removeClass('text-success').addClass('text-danger');
+        } else {
+            $('#length').removeClass('text-danger').addClass('text-success');
+        }
+        //check if entry contains numbers
+        if (name.match(/^([^0-9]*)$/)) {
+            $('#number').removeClass('text-danger').addClass('text-success');
+        } else {
+            $('#number').removeClass('text-success').addClass('text-danger');
+        }
+    }).focus(function () {
+        $('#first').show();
+    }).blur(function () {
+        $('#first').hide();
+        });
+
+    $('#lastName').keyup(function () {
+        var name = $(this).val();
+        //check entry length
+        if (name.length < 1) {
+            $('#lastlength').removeClass('text-success').addClass('text-danger');
+        } else {
+            $('#lastlength').removeClass('text-danger').addClass('text-success');
+        }
+        //check if entry contains numbers
+        if (name.match(/^([^0-9]*)$/)) {
+            $('#lastnumber').removeClass('text-danger').addClass('text-success');
+        } else {
+            $('#lastnumber').removeClass('text-success').addClass('text-danger');
+        }
+    }).focus(function () {
+        $('#last').show();
+    }).blur(function () {
+        $('#last').hide();
+    });
+
+    $('.attending').change(function () {
+        var attending = $(this).val();
+        if (attending === 'Yes') {
+            $('#date').show();
+            $('#plusOne').show();
+            $('#farewell').hide();
+        } else {
+            $('#farewell').show();
+            $('#date').hide();
+            $('#plusOne').hide();
+        }
+    });
+
+    $('.guest').change(function () {
+        var plusOne = $(this).val();
+        if (plusOne === 'Yes') {
+            $('#plusOneInfo').show();
+            $('#guestNo').addClass('.hide');
+        } else {
+            $('#plusOneInfo').hide();
+            $('#guestYes').addClass('.hide');
+        }
+    });
+    
+});
