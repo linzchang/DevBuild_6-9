5b2989f844476f36ab801d26d8149e3a4f99b70a
Lin-z Chang <lin-zchang@amrock.com>
2018-12-14 13:58:24 -0500

    Added RegisterModel and LoginModel classes

---
+++ Assessment6/Models/RegistrationModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assessment6.Models
+{
+    public class RegistrationModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Attending { get; set; }
+
+        //Optional: (depends on whether or not they're attending party)
+        public string PartyDate { get; set; }
+        public string PlusOne { get; set; }
+        public string PlusOneName { get; set; }
+    }
+}
